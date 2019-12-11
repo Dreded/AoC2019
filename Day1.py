7d9ceceaeef74357599d058f8c7906e3fd857f38
Ryan H <dreded@dredx.com>
2019-12-10 23:59:10 -0800

    Created Git

---
+++ Day1.py
@@ -0,0 +1,22 @@
+class SantaFuel():
+    def __init__(self):
+        with open(r'Day1_input.txt', 'r') as f:
+            self.input = list(map(int,f.read().strip().split()))
+
+    def PartOne(self):
+        fuel = 0
+        for mass in self.input:
+            fuel += int(mass/3-2)
+        return fuel
+
+    def PartTwo(self):
+        fuel = 0
+        for mass in self.input:
+            while int(mass/3-2) > 0:
+                mass = int(mass/3-2)
+                fuel += mass
+        return fuel
+
+answer = SantaFuel()
+print("PartOne: {}".format(answer.PartOne()))
+print("PartTwo: {}".format(answer.PartTwo()))
---
+++ Day2.py
@@ -0,0 +1,26 @@
+'''
+Opcodes
+99 = end
+1 = Add from 2 positions and store in 3rd positions
+2 = Multiply
+3 #'s after opcode tell you these 3 positions
+For example, if your Intcode computer encounters 1,10,20,30, it should read the
+values at positions 10 and 20, add those values, and then overwrite the value at
+position 30 with their sum.
+
+move to opcode by stepping forward 4 positions(pos 1,2,3 would be locations)
+
+'''
+input = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,9,19,1,19,5,23,1,13,23,27,1,27,6,31,2,31,6,35,2,6,35,39,1,39,5,43,1,13,43,47,1,6,47,51,2,13,51,55,1,10,55,59,1,59,5,63,1,10,63,67,1,67,5,71,1,71,10,75,1,9,75,79,2,13,79,83,1,9,83,87,2,87,13,91,1,10,91,95,1,95,9,99,1,13,99,103,2,103,13,107,1,107,10,111,2,10,111,115,1,115,9,119,2,119,6,123,1,5,123,127,1,5,127,131,1,10,131,135,1,135,6,139,1,10,139,143,1,143,6,147,2,147,13,151,1,5,151,155,1,155,5,159,1,159,2,163,1,163,9,0,99,2,14,0,0]
+opcode = 0
+pos = 0
+input[1] = 12
+input[2] = 2
+while input[pos] != 99:
+    opcode = input[pos]
+    if opcode == 1:
+        input[input[pos+3]] = input[input[pos+1]] + input[input[pos+2]]
+    elif opcode == 2:
+        input[input[pos+3]] = input[input[pos+1]] * input[input[pos+2]]
+    pos += 4
+print(input[0])
